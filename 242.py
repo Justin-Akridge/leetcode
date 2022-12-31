2c437edb46cd068075e8bca2d719dfe57d986927
Justin-Akridge <akridjus@gmail.com>
2022-12-31 14:34:14 -0500

    242

---
+++ 242.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        d = {}
+        for ch in s:
+            if ch not in d:
+                d[ch]=1
+            else:
+                d[ch]+=1
+        print(d)
+        for ch in t:
+            if ch in d:
+                if d[ch]>1:
+                    d[ch]-=1
+                else:
+                    del d[ch]
+            else:
+                return False
+        return d == {}
