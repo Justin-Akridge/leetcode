a4df475318297760c9a7a820d9d801e317677d48
Justin-Akridge <akridjus@gmail.com>
2022-12-31 14:50:49 -0500

    Valid Anagram

--- 242.py
+++ 242.py
@@ -1,11 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        # Create hashmap and add values to it
         d = {}
         for ch in s:
             if ch not in d:
                 d[ch]=1
             else:
                 d[ch]+=1
+                
+        # Search hashmap for existing values in string t
+        # delete values if values are same
         for ch in t:
             if ch in d:
                 if d[ch]>1:
