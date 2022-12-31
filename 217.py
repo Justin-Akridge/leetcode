c73254025264fbb01e99aa0c665fd0d543162b86
Justin-Akridge <akridjus@gmail.com>
2022-12-31 15:01:42 -0500

    Contains Duplicate

--- 217.py
+++ 217.py
@@ -1,10 +1,17 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-      #implementing solution with hashset
+        #implementing solution with hashset
         hset = set()
         for i in nums:
             if i in hset:
                 return True
             else:
                 hset.add(i)
+
+        #two pointer
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i-1]:
+                return True
+        return False        
             
