9da7e84748691650af4aecc74f608090c62b8197
Justin-Akridge <akridjus@gmail.com>
2023-01-01 10:43:58 -0500

    Missing number

---
+++ 268.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int ans = nums.size();
+        int i = 0;
+        sort(nums.begin(), nums.end());
+        while(i < nums.size()){
+            if (nums[i] == i){
+                i += 1;
+            }
+            else{
+                ans = i;
+                break;
+            }
+        }
+        return ans;
+    }
+};
