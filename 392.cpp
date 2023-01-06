6eb834918a0c53251bbc90f9834c646669e73111
Justin-Akridge <akridjus@gmail.com>
2023-01-05 21:20:24 -0500

    Is Subsequence

--- 392.cpp
+++ 392.cpp
@@ -5,12 +5,13 @@ using namespace std;
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int j=0;
-        for(int i=0; i<t.size(); ++i){
-            if (s[j] == t[i]){
-                j++;
+        int i=0, j=0;
+        while(i<s.size() && j<t.size()){
+            if(s[i] == t[j]){
+                i++;
             }
+            j++;
         }
-        return j == s.size();
+        return i == s.size();
     }
 };
