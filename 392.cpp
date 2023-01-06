90417cad012f5bba4ccd9cf6977b6155be43df7b
Justin-Akridge <akridjus@gmail.com>
2023-01-05 21:24:20 -0500

    Subsequence

--- 392.cpp
+++ 392.cpp
@@ -2,16 +2,18 @@
 #include <string>
 using namespace std;
 
-class Solution {
-public:
-    bool isSubsequence(string s, string t) {
-        int i=0, j=0;
-        while(i<s.size() && j<t.size()){
-            if(s[i] == t[j]){
-                i++;
-            }
-            j++;
-        }
-        return i == s.size();
-    }
-};
+int main(){
+  class Solution {
+  public:
+      bool isSubsequence(string s, string t) {
+          int i=0, j=0;
+          while(i<s.size() && j<t.size()){
+              if(s[i] == t[j]){
+                  i++;
+              }
+              j++;
+          }
+          return i == s.size();
+      }
+  };
+}
