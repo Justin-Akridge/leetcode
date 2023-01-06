913c2a6810a44f99c654fe76e61ddb91390eb25f
Justin-Akridge <akridjus@gmail.com>
2023-01-05 21:22:59 -0500

    Missing Number

--- 268.cpp
+++ 268.cpp
@@ -1,18 +1,25 @@
-class Solution {
-public:
-    int missingNumber(vector<int>& nums) {
-        int ans = nums.size();
-        int i = 0;
-        sort(nums.begin(), nums.end());
-        while(i < nums.size()){
-            if (nums[i] == i){
-                i += 1;
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    class Solution {
+    public:
+        int missingNumber(vector<int>& nums) {
+            int ans = nums.size();
+            int i = 0;
+            sort(nums.begin(), nums.end());
+            while(i < nums.size()){
+                if (nums[i] == i){
+                    i += 1;
+                }
+                else{
+                    ans = i;
+                    break;
+                }
             }
-            else{
-                ans = i;
-                break;
-            }
+            return ans;
         }
-        return ans;
-    }
-};
+    };
+}
