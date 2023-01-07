e1a2e2e0e4b31c5fdb563cb997370e797635eea1
Justin-Akridge <akridjus@gmail.com>
2023-01-07 10:49:18 -0500

    Valid Parenthese

--- 20.py
+++ 20.py
@@ -1,15 +1,12 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        stk = []
-        if len(s) == 1:
-            return False
-        for i in range(len(s)):
-            if s[i] == '(' or s[i] == '{' or s[i] == '[':
-                stk.append(s[i])
-            elif s[i] == ')' or s[i] == '}'  or s[i] == ']':
-                if stk == []:
-                    return False
-                t = stk.pop()
-                if s[i] == ')' and t != '(' or s[i] == '}' and t != '{' or s[i] == ']' and t != '[':
-                    return False
-        return stk == []
+        stack = []
+        closeToOpen = { ")" : "(", "]" : "[", "}": "{" }
+        for i in s:
+            if i in closeToOpen:
+                if stack and stack[-1] == closeToOpen[i]:
+                    stack.pop()
+                else: return False
+            else:
+                stack.append(i)
+        return True if not stack else False
