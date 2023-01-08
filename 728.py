530edf0a751d51fa8cfc664d719cbcd88215d1e3
Justin-Akridge <akridjus@gmail.com>
2023-01-08 15:27:51 -0500

    intersection of two arrays

--- 728.py
+++ 728.py
@@ -1,13 +1,12 @@
-class Solution:
-    def selfDividingNumbers(self, left: int, right: int) -> List[int]:
-        ans = []
-        while left <= right:
-            l = [int(x) for x in str(left)]
-            sum = 0
-            for i in range(len(l)):
-                if l[i] != 0 and left % l[i] == 0:
-                    sum+=1
-            if sum == len(l):
-                ans.append(left)
-            left+=1
-        return ans
+def distinctPrimeFactors(nums):
+        nums.sort(reverse=True)
+        c = 0
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] > 1 and nums[i] % 1 == 0 and nums[i] % nums[i] == 0:
+                    ans = nums[i] / nums[j]
+                    if ans.is_integer():
+                        c+=1
+                        break
+        return c
+distinctPrimeFactors([2,4,3,7,10,6])
