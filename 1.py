8b12dcbc9e0fee1d11121a8dedcff0006f2151dc
Justin-Akridge <akridjus@gmail.com>
2023-01-10 20:37:41 -0500

    two sum cpp

--- 1.py
+++ 1.py
@@ -6,3 +6,23 @@ class Solution:
             if compliment in hashmap:
                 return [i, hashmap[compliment]]
             hashmap[nums[i]] = i
+
+#cpp solution
+# class Solution {
+# public:
+#     vector<int> twoSum(vector<int>& nums, int target) {
+#         unordered_map<int, int> mp;
+#         vector<int> result;
+#         for(int i=0; i<nums.size(); i++){
+#             int complement = target - nums[i];
+#             if (mp.find(complement) != mp.end()){
+#                 result.push_back(mp[complement]);
+#                 result.push_back(i);
+#                 break;
+#             } else{
+#                 mp.insert({nums[i], i});
+#             }
+#         }
+#         return result;
+#     }
+# };
