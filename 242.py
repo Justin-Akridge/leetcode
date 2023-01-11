985cfd388e2f3fdeaf502544e0c9b32c8fc68a17
Justin-Akridge <akridjus@gmail.com>
2023-01-10 20:29:40 -0500

    Add cpp solution

--- 242.py
+++ 242.py
@@ -19,3 +19,18 @@ class Solution:
             else:
                 return False
         return d == {}
+
+        
+# cpp solution:
+# class Solution {
+# public:
+#     bool isAnagram(string s, string t) {
+#         unordered_map<char,int> mp;
+#         for (auto ch: s)mp[ch]++;
+#         for (auto ch: t){
+#             mp[ch]--;
+#             if (mp[ch]==0)mp.erase(ch);
+#         }
+#         return mp.empty();
+#     }
+# };
